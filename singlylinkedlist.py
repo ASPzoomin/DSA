2e0ce151f598f297c02eef2b5cd6444b8bc8e9f5
saipavan <pavanammula@gmail.com>
2019-02-17 11:49:39 +0530

    sdfsd

---
+++ singlylinkedlist.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class Linkedlist:
+    def __init__(self):
+        self.head=None
+    def insert(self,newnode):
+        if self.head is None:
+            self.head=newnode
+        else:
+            lastnode=self.head
+            while True:
+                if lastnode.next is None:
+                    break
+                lastnode=lastnode.next
+            lastnode.next=newnode
+    def print(self):
+        currentnode=self.head
+        while True:
+            if currentnode is None:
+                break
+            print(currentnode.data)
+            currentnode=currentnode.next
+firstnode=Node("pavann")
+linkedlist=Linkedlist()
+linkedlist.insert(firstnode)
+secondnode=Node("lakshmipriya")
+linkedlist.insert(secondnode)
+thirdnode=Node("sreekar")
+linkedlist.insert(thirdnode)
+linkedlist.print()
+
+
